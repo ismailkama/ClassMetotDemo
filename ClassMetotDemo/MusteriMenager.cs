111bbd720c776d501fdcac0348fb193bc3ddb72d
kamaismail <76253191+kamaismail@users.noreply.github.com>
2021-01-22 15:52:33 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriMenager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriMenager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Hoşgeldiniz Sayın" + " " + musteri.MusteriAdi + " " + musteri.MusteriSoyadi + " " + musteri.MusteriYasi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "Adem";
+            musteri1.MusteriSoyadi = "Büyük";
+            musteri1.MusteriYasi = 22;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "Ali";
+            musteri2.MusteriSoyadi = "Yıldız";
+            musteri2.MusteriYasi = 35;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriAdi = "Eylül";
+            musteri3.MusteriSoyadi = "Kahraman";
+            musteri3.MusteriYasi = 28;
+
+            MusteriMenager musteriMenager = new MusteriMenager();
+            musteriMenager.Ekle(musteri1);
+            musteriMenager.Ekle(musteri2);
+            musteriMenager.Ekle(musteri3);
+
+        }
+    }
+}
